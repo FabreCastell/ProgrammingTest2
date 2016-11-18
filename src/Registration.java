b952fb27c820618f7b899516e45d3ce1c4714c2a
FaberCastell <vorason.si@gmail.com>
2016-11-18 15:11:00 +0700

    done registration

--- src/Registration.java
+++ src/Registration.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Registration {
     // Request repository
@@ -7,37 +8,47 @@ public class Registration {
     // Add new course, only request with non-exists studentID/courseID
     // is a valid request. Invalid request will cause an exception
     public void add(IRequest request) {
+        if (exists(request) ){
+            throw new RuntimeException("exists studentID or courseID");
+        }else{
+            requestList.add(request);
+        }
 
     }
 
     // Drop existing course, only request with existing studentID/courseID
     // is a valid request. Invalid request will cause an exception
     public void drop(IRequest request) {
+        if (!exists(request)){
+            throw new RuntimeException("not match studentID or courseID");
+        }else{
+            requestList.add(request);
+        }
 
     }
 
     // Check whether the request is in repository.
     public boolean exists(IRequest request) {
-        return false;
+        return requestList.contains(request);
     }
 
     // Count all requests
     public int count() {
-        return 0;
+        return requestList.size();
     }
 
     // Count requests by request type
     public int countByType(int type) {
-        return 0;
+        return Collections.frequency(requestList, type);
     }
 
     // Count request by course ID
     public int countByCourseID(String courseID) {
-        return 0;
+        return Collections.frequency(requestList, courseID);
     }
 
     // Count request by student ID
     public int countByStudentID(String studentID) {
-        return 0;
+        return Collections.frequency(requestList, studentID);
     }
 }
